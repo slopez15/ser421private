07c00dd9a469f2069247e5c87e4213f9e742d5da
Saul Lopez <slopez15@asu.edu>
2018-10-25 14:50:07 -0700

    ajax and webSec in GH yet. android_ex in code folder--too long for git.

--- lab4/fromGH/NodeIntro/1. notWant/simplenexttick.js
+++
@@ -1,8 +0,0 @@
-console.log('start');
-process.nextTick(() => {
-  console.log('nextTick callback');
-});
-console.log('scheduled');
-function hello() { console.log('hello world'); }
-hello();
-process.nextTick(hello);
